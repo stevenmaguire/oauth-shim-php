6bf53c66c6bd91a02f13788490ad19757571ff6e
Steven Maguire <steven@stevenmaguire.com>
2014-10-20 21:16:00 -0500

    debugging in the open

--- src/Stevenmaguire/Oauth/OAuthUtil.php
+++ src/Stevenmaguire/Oauth/OAuthUtil.php
@@ -5,7 +5,7 @@ class OAuthUtil
     public static function urlencode_rfc3986($input)
     {
         if (is_array($input)) {
-            return array_map(array('OAuthUtil', 'urlencode_rfc3986'), $input);
+            return array_map(array('Stevenmaguire\Oauth\OAuthUtil', 'urlencode_rfc3986'), $input);
         } else if (is_scalar($input)) {
             return str_replace(
                 '+',
